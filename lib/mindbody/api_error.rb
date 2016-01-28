246823c1b38989bdadbc1fcdb32e7b3ca568a411
Benjamin Moss <drteeth@gmail.com>
2016-01-28 00:50:00 -0500

    ApiError now takes a result object

--- lib/mindbody/api_error.rb
+++ lib/mindbody/api_error.rb
@@ -1,6 +1,10 @@
 class MindBody::ApiError < StandardError
+  attr_reader :status, :error_code, :error_message
 
   def initialize(result)
+    @status = result.status
+    @error_code = result.error_code
+    @error_message = result.error_message
     super("#{result.status} (#{result.error_code}): #{result.error_message}")
   end
 
