f1e6a77378b95b65ce4c3cf2738a0d3ca3957b8c
Benjamin Moss <drteeth@gmail.com>
2016-02-24 17:01:54 -0500

    make CheckoutError confirm to ApiError's interface

--- lib/mindbody/checkout_error.rb
+++ lib/mindbody/checkout_error.rb
@@ -1,9 +1,18 @@
 class MindBody::CheckoutError < MindBody::ApiError
-  attr_reader :errors
 
   def initialize(result, checkout_errors)
     super(result)
     @errors = Array.wrap(checkout_errors)
+  end
+
+  def error_message
+    if @errors.any?
+      @errors.map do |error|
+        error[:string].gsub(/Item \d+ -/, '')
+      end.join(" ")
+    else
+      @error_message
+    end
   end
 
 end
